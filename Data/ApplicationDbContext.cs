692de8349c008dcc1c719b591a686bef851dfd82
Ahmed <AhmedQasim13>
2025-01-24 11:03:43 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDb.Models;
+
+namespace SalesDb.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        public DbSet<Customer>? Customers { set; get; }
+        public DbSet<Product>? Products { set; get; }
+        public DbSet<Sale>? Sales { set; get; }
+        public DbSet<Store>? Stores { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFSalesDatabase;Integrated Security=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Customer Table
+            modelBuilder.Entity<Customer>()
+                .HasKey(p => p.CustomerId);
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsUnicode(true);
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            //Product Table
+            modelBuilder.Entity<Product>()
+                .HasKey(p => p.ProductId);        
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)                 
+                .IsUnicode(true);
+            //Store Table
+            modelBuilder.Entity<Store>()
+                .HasKey(p => p.StoreId);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(80)
+                .IsUnicode(true);
+            //Sale Table 
+            modelBuilder.Entity<Sale>()
+                .HasKey(p => p.SaleId);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDb.Models
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public String Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public List<Sale> Sales { get; set; }
+
+    }
+}
